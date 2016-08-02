ef7b0d9ca1b6212fd0daa11d4603ebc646e7b5e9
lelefly <18501250210@163.com>
2016-08-02 16:46:25 +0800

    main commit

--- app/src/main/java/com/example/administrator/test/MainActivity.java
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -38,6 +38,8 @@ public class MainActivity extends Activity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("testlog", "refresh");
+                Log.i("testlog", "refresh");
                 startActivity(new Intent(MainActivity.this, OtherActivity.class));
             }
         });
