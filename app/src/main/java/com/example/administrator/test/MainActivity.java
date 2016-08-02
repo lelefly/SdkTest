310cd4c742335c70963a5d16b9a6214eb4abf517
lelefly <18501250210@163.com>
2016-08-02 17:00:03 +0800

    Merge branch 'test'
    # Conflicts:
    #	app/src/main/java/com/example/administrator/test/MainActivity.java

--- app/src/main/java/com/example/administrator/test/MainActivity.java
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -38,8 +38,8 @@ public class MainActivity extends Activity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("testlog", "last");
-                Log.i("testlog", "last");
+                Log.i("testlog", "tttllll");
+                Log.i("testlog", "tttllll");
                 startActivity(new Intent(MainActivity.this, OtherActivity.class));
             }
         });
