580a7377adbbc59d4197064dae35fa5b246d31e1
lelefly <18501250210@163.com>
2016-08-02 16:53:33 +0800

    test ttt

--- app/src/main/java/com/example/administrator/test/MainActivity.java
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -38,8 +38,8 @@ public class MainActivity extends Activity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("testlog", "1");
-                Log.i("testlog", "2");
+                Log.i("testlog", "ttttt");
+                Log.i("testlog", "tttttt");
                 startActivity(new Intent(MainActivity.this, OtherActivity.class));
             }
         });
